35637f3d6adbe9812e6b87e326b04acf06ad0027
Jie Liu <cbuilder1993@gmail.com>
2016-09-20 04:55:29 +0800

    this is the util for the intelDB

---
+++ intelDB_util1.erl
@@ -0,0 +1,36 @@
+%% @author Administrator
+%% @doc @todo Add description to intelDB_util1.
+
+
+-module(intelDB_util1).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([getVersion/0]).
+
+
+-define(VERSION, 1000000).
+-define(MAX_FILESIZE, 512*1024*1024).
+-define(FILE_PREX, "Files").
+
+getVersion() ->
+	%%Var = ?VERSION,
+	?FILE_PREX.
+
+
+ensureDir(Dir, Default) ->
+	NewDir =case Dir of
+	   "" -> Default;
+	   _ -> Dir
+	end,
+	case filelib:ensure_dir(NewDir) of
+         ok -> {ok, NewDir};
+         {error, Reason} -> {error, Reason}
+    end.
+
+
+
+stamptomsecs({MegaSecs,Secs,MicroSecs} = Timestamp) ->
+  (MegaSecs*1000000 + Secs)*1000000 + MicroSecs.	
+	
